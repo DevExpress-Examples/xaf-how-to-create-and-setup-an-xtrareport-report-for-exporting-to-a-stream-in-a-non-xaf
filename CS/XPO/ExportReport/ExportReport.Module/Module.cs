5091f770bc5129e10f5e07b19c452b64925f59ac
andrey <andrey.kozhevnikov@devexpress.com>
2023-06-14 15:52:22 +0400

    add report

--- CS/XPO/ExportReport/ExportReport.Module/Module.cs
+++ CS/XPO/ExportReport/ExportReport.Module/Module.cs
@@ -12,6 +12,10 @@ using DevExpress.ExpressApp.Model.DomainLogics;
 using DevExpress.ExpressApp.Model.NodeGenerators;
 using DevExpress.Xpo;
 using DevExpress.ExpressApp.Xpo;
+using DevExpress.ExpressApp.ReportsV2;
+using DevExpress.XtraReports.UI;
+using ExportReportDemo.Module.Reports;
+using ExportReportDemo.Module.BusinessObjects;
 
 namespace ExportReport.Module;
 
@@ -27,7 +31,10 @@ public sealed class ExportReportModule : ModuleBase {
     }
     public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB) {
         ModuleUpdater updater = new DatabaseUpdate.Updater(objectSpace, versionFromDB);
-        return new ModuleUpdater[] { updater };
+        PredefinedReportsUpdater predefinedReportsUpdater =
+      new PredefinedReportsUpdater(Application, objectSpace, versionFromDB);
+        predefinedReportsUpdater.AddPredefinedReport<EmployeesReport>("Contacts Report", typeof(Employee));
+        return new ModuleUpdater[] { updater, predefinedReportsUpdater };
     }
     public override void Setup(XafApplication application) {
         base.Setup(application);
